88eed4e8c13f5465091e9e9afdb363f9fbd2ca6f
unknown <jpc@jpc-PC.(none)>
2012-04-30 21:10:38 -0500

    added simple validation on purchased_time, ticket_time

--- app/models/receipt.rb
+++ app/models/receipt.rb
@@ -1,5 +1,10 @@
 class Receipt < ActiveRecord::Base
   attr_accessible :email, :expiration_time, :purchased_time, :rate
+  validate :purchased_time_is_valid
+
+  def purchased_time_is_valid
+    errors.add(:purchased_time, 'purchased time is not Right, please double check') if purchased_time >= Time.now
+  end
 
   def self.search(search)
     self.where(search.conditions)
--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,3 +1,9 @@
 class Ticket < ActiveRecord::Base
   attr_accessible :email, :ticket_time, :rate, :cell_number
+  validate :ticket_time_is_valid
+
+  def ticket_time_is_valid
+    errors.add(:ticket_time, 'ticket time is not Right, please double check') if ticket_time >= Time.now
+  end
+
 end
