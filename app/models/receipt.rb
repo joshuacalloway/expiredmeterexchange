eb87d592159b29ca0858a3bb1236c64b643d58b6
unknown <jpc@jpc-PC.(none)>
2012-05-01 00:03:11 -0500

    added expiration_time validation

--- app/models/receipt.rb
+++ app/models/receipt.rb
@@ -1,6 +1,10 @@
 class Receipt < ActiveRecord::Base
   attr_accessible :email, :expiration_time, :purchased_time, :rate
-  validate :purchased_time_is_valid, :email_is_valid
+  validate :purchased_time_is_valid, :email_is_valid, :expiration_time_is_valid
+
+  def expiration_time_is_valid
+    errors.add(:expiration_time, 'expiration time is not Right, please double check') if expiration_time <= purchased_time
+  end
 
   def purchased_time_is_valid
     errors.add(:purchased_time, 'purchased time is not Right, please double check') if purchased_time >= Time.now
