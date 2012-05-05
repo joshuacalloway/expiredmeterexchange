0c8c372b4fec5e694011bf27825c7ee35a420def
unknown <jpc@jpc-PC.(none)>
2012-05-05 07:52:10 -0500

    moved validation of email to helper

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,12 +1,13 @@
 class Transaction < ActiveRecord::Base
   attr_accessible :buyer_email, :receipt_id, :status
-  validates_presence_of :buyer_email
-  validate :buyer_email_is_valid
+  validates :buyer_email, :presence => true, :email_format => true
+
+#  validates_presence_of :buyer_email
+#  validate :buyer_email_is_valid
 
 
-  def buyer_email_is_valid
-    errors.add(:buyer_email, 'email is not right.  It should be in format of XXXX@gmail.com') unless buyer_email =~ /^[a-zA-Z][\w\.-]*[a-zA-Z0-9]@[a-zA-Z0-9][\w\.-]*[a-zA-Z0-9]\.[a-zA-Z][a-zA-Z\.]*[a-zA-Z]$/
-  end
-
+#  def buyer_email_is_valid
+#    errors.add(:buyer_email, 'email is not right.  It should be in format of XXXX@gmail.com') unless buyer_email =~ /^[a-zA-Z][\w\.-]*[a-zA-Z0-9]@[a-zA-Z0-9][\w\.-]*[a-zA-Z0-9]\.[a-zA-Z][a-zA-Z\.]*[a-zA-Z]$/
+#  end
 
 end
