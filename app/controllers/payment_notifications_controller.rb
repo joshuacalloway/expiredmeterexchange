cfb314f198c69c0a148ae85ce5f29cf22c86d410
unknown <jpc@jpc-PC.(none)>
2012-05-05 15:07:28 -0500

    took out email when paying at paypal.  can get that from IPN

--- app/controllers/payment_notifications_controller.rb
+++ app/controllers/payment_notifications_controller.rb
@@ -5,6 +5,9 @@ class PaymentNotificationsController < ApplicationController
   # POST /payment_notifications.json
   def create
     PaymentNotification.create!(:params => params, :transaction_id => params[:invoice], :status => params[:payment_status], :txn_id => params[:txn_id])
+    transaction = Transaction.find(params[:invoice])
+    transaction.buyer_email = params[:payer_email]
+    transaction.save
     render :nothing => true
   end
 end
