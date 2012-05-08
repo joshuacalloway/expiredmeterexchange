12a098e6878529af4573bc0b00fea4efae781241
unknown <jpc@jpc-PC.(none)>
2012-05-08 15:56:56 -0500

    made email for matched ticket link to receipt

--- app/controllers/receipts_controller.rb
+++ app/controllers/receipts_controller.rb
@@ -14,6 +14,8 @@ class ReceiptsController < ApplicationController
   # GET /receipts/1.json
   def show
     @receipt = Receipt.find(params[:id])
+    @transaction = Transaction.new
+    @transaction.receipt_id = @receipt.id
 
     respond_to do |format|
       format.html # show.html.erb
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -46,4 +46,5 @@ Ptrexchange::Application.configure do
     :authentication       => 'plain',
     :enable_starttls_auto => true  
   }
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
