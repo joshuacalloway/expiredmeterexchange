5e5132a33338357b857051318e51ead498aa4a14
unknown <jpc@jpc-PC.(none)>
2012-05-08 16:25:46 -0500

    email settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,6 +51,17 @@ Ptrexchange::Application.configure do
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :domain               => 'gmail.com',
+    :user_name            => 'joshua.calloway@gmail.com',
+    :password             => 'call00way',
+    :authentication       => 'plain',
+    :enable_starttls_auto => true  
+  }
+
   # Enable threaded mode
   # config.threadsafe!
 
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -29,6 +29,19 @@ Ptrexchange::Application.configure do
   # ActionMailer::Base.deliveries array.
   config.action_mailer.delivery_method = :test
 
+
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :domain               => 'gmail.com',
+    :user_name            => 'joshua.calloway@gmail.com',
+    :password             => 'call00way',
+    :authentication       => 'plain',
+    :enable_starttls_auto => true  
+  }
+
   # Raise exception on mass assignment protection for Active Record models
   config.active_record.mass_assignment_sanitizer = :strict
 
