ae2fac98fb1b0eb2bb077154ac6becbc1e47f866
unknown <jpc@jpc-PC.(none)>
2012-05-09 12:17:43 -0500

    using helpers in receipt to clean up

--- app/helpers/receipts_helper.rb
+++ app/helpers/receipts_helper.rb
@@ -1,2 +1,33 @@
 module ReceiptsHelper
+  def hour_select(id, name, cssclass='')
+    subhtml=""
+    (0..23).each { |hour|
+      subhtml << "<option value='" << leading_zero(hour) << "'>" <<  leading_zero(hour) << "</option>"
+    }
+    html = "<select class='#{cssclass}' id='#{id}' name='#{name}'>#{subhtml}</select>"
+    render(:inline=> html)
+  end
+
+  def minute_select(id, name, cssclass='')
+    subhtml=""
+    (0..59).each { |minute|
+      subhtml << "<option value='" << leading_zero(minute) << "'>" <<  leading_zero(minute) << "</option>"
+    }
+    html = "<select class='#{cssclass}' id='#{id}' name='#{name}'>#{subhtml}</select>"
+    render(:inline=> html)
+  end
+
+  def day_select(id, name, cssclass='')
+    subhtml=""
+    (1..31).each { |day|
+      subhtml << "<option value='" << leading_zero(day) << "'>" <<  leading_zero(day) << "</option>"
+    }
+    html = "<select class='#{cssclass}' id='#{id}' name='#{name}'>#{subhtml}</select>"
+    render(:inline=> html)
+  end
+    
+  def leading_zero(hour)
+    "%02d" % hour
+  end
+
 end
