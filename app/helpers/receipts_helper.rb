aee0159a267a302d15b72603dc1f835912ac6cfe
unknown <jpc@jpc-PC.(none)>
2012-05-11 00:09:24 -0500

    limit months to receipt entry to last 3 months

--- app/helpers/receipts_helper.rb
+++ app/helpers/receipts_helper.rb
@@ -54,7 +54,9 @@ module ReceiptsHelper
 
   def month_select(id, name, selected_month = Time.now.mon, cssclass='')
     subhtml=""
-    (1..12).each { |month|
+    beginMon = Time.now.mon - 2
+    beginMon = 12-beginMon if beginMon < 0
+    (beginMon..Time.now.mon).each { |month|
       if (month == selected_month) 
         selected = "selected"
       else
@@ -75,7 +77,10 @@ module ReceiptsHelper
   def month_abbrev_select(id, name, selected_month = Time.now.mon, cssclass='')
     subhtml=""
     codes = {1 => "JAN", 2 => "FEB", 3 => "MAR", 4 => "APR", 5 => "MAY", 6 => "JUN", 7 => "JUL", 8 => "AUG", 9 => "SEP", 10 => "OCT", 11 => "NOV", 12 => "DEC" }
-    (1..12).each { |month|
+
+    beginMon = Time.now.mon - 2
+    beginMon = 12-beginMon if beginMon < 0
+    (beginMon..Time.now.mon).each { |month|
       if (month == selected_month) 
         selected = "selected"
       else
