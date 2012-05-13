b288a815be46f834f1d76a2d9942f410de348c03
unknown <jpc@jpc-PC.(none)>
2012-05-13 18:26:30 -0500

    fixed some styling bugs

--- app/helpers/receipts_helper.rb
+++ app/helpers/receipts_helper.rb
@@ -1,5 +1,5 @@
 module ReceiptsHelper
-  def hour_select(id, name, selected_hour = Time.now.hour, cssclass='')
+  def hour_military_select(id, name, selected_hour = Time.now.hour, cssclass='')
     subhtml=""
     (0..23).each { |hour|
       if (hour == selected_hour) 
@@ -8,6 +8,20 @@ module ReceiptsHelper
         selected = ""
       end
       subhtml << "<option value='" << leading_zero(hour) << "'" << selected << ">" <<  leading_zero(hour) << "</option>"
+    }
+    html = "<select class='#{cssclass}' id='#{id}' name='#{name}'>#{subhtml}</select>"
+    render(:inline=> html)
+  end
+
+  def hour_select(id, name, selected_hour = Time.now.hour, cssclass='')
+    subhtml=""
+    (0..23).each { |hour|
+      if (hour == selected_hour) 
+        selected = "selected"
+      else
+        selected = ""
+      end
+      subhtml << "<option value='" << leading_zero(hour) << "'" << selected << ">" <<  leading_zero(to_american_format(hour)) << "</option>"
     }
     #javascript =""
     javascript = "onchange='adjustAMPM()'"
@@ -19,6 +33,15 @@ module ReceiptsHelper
     render(:inline=> html)
   end
 
+  def to_american_format hour
+    if hour < 13
+      hour = hour
+    else
+      hour = hour - 12
+    end
+    hour
+  end
+    
   def minute_select(id, name, selected_minute = Time.now.min, cssclass='')
     subhtml=""
     (0..59).each { |minute|
